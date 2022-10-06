49fc8940ad7711199e5e2aa3b2e2f275549dce50
perebartrolisimo <pere.bartroli@ub.uu.se>
2022-10-06 15:28:36 +0000

    Separate spies in order to have less dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,10 @@
 /**
  * The testspies module provides common spies used when testing Cora projects.
  */
-module se.uu.ub.cora.testspies {
+module se.uu.ub.cora.testutils.spies {
 
 	requires org.testng;
-	/**
-	 * logger, data etc. are not transitive, as it is used for tests and all usages may not need
-	 * both, leaving them non transitive means we do not need to include the one we are not using
-	 */
-	requires se.uu.ub.cora.logger;
-	requires se.uu.ub.cora.data;
-	requires se.uu.ub.cora.spider;
-	requires se.uu.ub.cora.httphandler;
 	requires transitive se.uu.ub.cora.testutils;
 
-	exports se.uu.ub.cora.testspies.logger;
-	exports se.uu.ub.cora.testspies.data;
-	exports se.uu.ub.cora.testspies.spider;
-	exports se.uu.ub.cora.testspies.httphandler;
-	exports se.uu.ub.cora.testspies.storage;
+	exports se.uu.ub.cora.testutils.spies;
 }
--- src/main/java/se/uu/ub/cora/testutils/spies/MCRSpy.java
+++ src/main/java/se/uu/ub/cora/testutils/spies/MCRSpy.java
@@ -16,7 +16,7 @@
  *     You should have received a copy of the GNU General Public License
  *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.uu.ub.cora.testspies.spy;
+package se.uu.ub.cora.testutils.spies;
 
 import se.uu.ub.cora.testutils.mcr.MethodCallRecorder;
 import se.uu.ub.cora.testutils.mrv.MethodReturnValues;
